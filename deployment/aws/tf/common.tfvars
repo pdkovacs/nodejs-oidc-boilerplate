pdkovacs/nodejs-oidc-skeleton

Replace HTMX-PugJS with ReactJS

deployment/aws/tf/common.tfvars
@@ -1,1 +1,1 @@
-vpc_name              = "nodejs-skeleton-apigw-private-integration"
+vpc_name              = "nodejs-boilerplate-apigw-private-integration"

pdkovacs/nodejs-oidc-boilerplate

deployment/aws/tf/common.tfvars
@@ -1,1 +1,1 @@
-vpc_name              = "nodejs-skeleton-apigw-private-integration"
+vpc_name              = "node-boilerplate-apigw-private-integration"